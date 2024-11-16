1cd9abd794d3c2583513154abc15430cb3498832
Marcel Wijlaars <marcel@wijlaars.eu>
2024-11-16 08:04:00 +0100

    powerpc sync cleanup

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 #lscpu | grep 'Byte Order:'
 
 all:
-	gcc -Wunused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wall -o hpoa md5/md5.c hpoa.c 
+#	gcc -Wl,-emain -Wall -Wno-unused-variable -Wno-unused-but-set-variable -o hpoa md5/md5.c sha2/sha2.c hpoa.c
+	gcc -nostdlib -L../lib  -Wl,-dynamic-linker,ld-uClibc.so.0,-lc -Wall -Wno-unused-variable -Wno-unused-but-set-variable -o hpoa md5/md5.c sha2/sha2.c hpoa.c
+local:
+	gcc -nostdlib -I/usr/local/x86_64-linux-uclibc/usr/include -L/usr/local/x86_64-linux-uclibc/usr/lib -Wl,-emain,-dynamic-linker,libuClibc-1.0.50.so -Wall -Wno-unused-variable -Wno-unused-but-set-variable -o hpoa md5/md5.c sha2/sha2.c hpoa.c
+
+
 
 old2:
 	gcc -pedantic -Wunused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wall -o hpoa md5/md5.c hpoa.c -lcrypto
@@ -10,4 +15,4 @@ old:
 	gcc -pedantic -Wno-unused-variable -Wno-unused-but-set-variable -Wall -o hpoa hpoa.c sha2/sha2.c
 
 clean:
-	rm hpoa md5/md5.o
+	rm hpoa
--- hpoa.h
+++ hpoa.h
@@ -2,38 +2,32 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <endian.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-//#include "sha2/sha2.h"
-#include <openssl/sha.h>
+//#include "data_0x10010100.h" // needed fro sha stuff??
+#include "sha2/sha2.h"
+//#include <openssl/sha.h>
 #include "md5/md5.h"
-
-
-/*
- * offset locations from bytewalk:
- *
- * 0xD5,     found: 
- * 0x115
- * 0x173038, found:
- * 0x173078
- * 0x4E5CD9, found:
- * 0xCD7CFC
- * 0xCD7D3C
-*/
 
 #define HEADER_SIZE 0xD5
 #define MAX_FILE_SIZE 0xFFFFFF
 #define C7000 2
 #define C3000 1 //actually 1 or 3
 #define OA_BOARD_TYPE C7000
-/*
- * define endiness,
- * LITTLE_ENDIAN: LE=1   
- * BIG_ENDIAN:    LE=0
- */
+
+
+#define __bswap_constant_32(x)						\
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |                      \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+
+# define __bswap_32(x) \
+     (__extension__                                                              \
+      ({ register unsigned int __x = (x); __bswap_constant_32 (__x); }))
+
+
+#define htobe32(x) __bswap_32 (x)
 
 // #define SEEK_SET 0
 // #define SEEK_CUR 1
@@ -58,35 +52,30 @@ const int __i__ = 1;
 #define CYAN    "\x1b[1;36m"
 #define DEFAULT "\x1b[1;0m"
 
-unsigned char check_something(int,int);
 
+uint open_mtd_for_output_internal(int,char*,int); /* FUN_10001edc */
+int open_mtd_for_input_internal(char*, int, void*); /* FUN_10002160 */
+int FUN_1000f7dc(unsigned int, unsigned int);
 char *partition_selector(unsigned char); /* FUN_100033a8 */
+void do_housekeeping(void);
+void do_sha256_tests(void);
 
+
+
+
+#if 0
+unsigned char check_something(int,int);
 unsigned char FUN_10005f64(unsigned char *, uint, unsigned char*, size_t, int, int);
-
-
 size_t FUN_10005d64(int, void *, size_t, SHA256_CTX *);
-
 uint64_t  fw_with_fingerprint(void);                       /* FUN_1000cb68 */ 
-
 uint32_t FUN_10005de8(int, unsigned char *, unsigned char *);
 uint32_t FUN_100089b8(int, uint *, int, int, int *);
 uint32_t FUN_1000a4c0(int,int,int);
-
-
 uint FUN_10009824(int, int, uint, uint);
 uint FUN_10009924(int, int, uint, int);
 uint FUN_1000610c(int, __off_t *, unsigned char *, int, int);
 uint FUN_10005cdc(int, uint, SHA256_CTX *);
-#if 0
-uint MD5_processing(char *,void *,size_t);
-void MD5_encryption(unsigned int*,unsigned int *,unsigned int);
-static void MD5_follow_precomputed_table(unsigned int *,unsigned int*);
-void part_of_MD5_calculations(int*, int*, uint);
-#endif
-
 void FUN_CONCAT(unsigned int*,unsigned int*,unsigned int); /* FUN_1000810 */
-void MD5_initialize_variables(unsigned int *);
 void kind_of_memset(unsigned int*,unsigned char,unsigned int); /* FUN_10008264 */
 void kind_of_memcpy(unsigned int*,unsigned int*,unsigned int); /* FUN_100081f0 */
 void FUN_10006944(unsigned int*,unsigned int*);
@@ -107,20 +96,9 @@ void FUN_1000b104(int*, int*, uint);
 uint FUN_1000aa18(uint);
 uint FUN_1000a738(int, int, uint, int, uint);
 void MD5_printf(unsigned int*,char*);
-
-
-
-int FUN_1000f7dc(unsigned int, unsigned int);
 int FUN_1000a8a4(int, int, uint, int, uint);
 int FUN_10009580(int, int, int, uint);
 int FUN_10008474(void *, size_t *, int, uint, int *);
 int FUN_1000a6bc(int,int);
-
-uint open_mtd_for_output_440(int, char*, int, int, int, unsigned int, unsigned int, unsigned int); /* FUN_10002a9c */
-uint open_mtd_for_output_130(int, char*, int, int, int, unsigned int, unsigned int, unsigned int); /* FUN_10001d2c */
-uint open_mtd_for_output_internal(int,char*,int); /* FUN_10001edc */
-
-int open_mtd_for_input_440(char*, int, void*); /* FUN_10002dd0 */
-int open_mtd_for_input_130(char*, int, void*);  /* FUN_10002020 */
-int open_mtd_for_input_internal(char*, int, void*); /* FUN_10002160 */
 int get_em_type(void);
+#endif
